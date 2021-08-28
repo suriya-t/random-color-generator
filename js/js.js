e4a521700201e19a94fb16c7fd26479c147e6d19
suriya-t <86153076+suriya-t@users.noreply.github.com>
2021-08-28 15:34:59 +0530

    Update js.js
    Added a randomColor() function to generate a random color and change the background color to it.
    Added comments.

--- js/js.js
+++ js/js.js
@@ -3,11 +3,23 @@ const colorText = document.getElementById("color_text");
 const mainSection = document.getElementById("main-section");
 
 function colorChange(){
+    /* Change the color of the background and color code. */
     mainSection.style.backgroundColor =  colorText.value = color.value;
 }
 
 function copyText(){
+    /* Copy the color to the clipboard. */
     colorText.focus();
     colorText.select();
     document.execCommand('copy');
+}
+
+function randomColor(){
+    /* Generating Random Color. */
+    var letters = "0123456789ABCDEF";
+    var hex = "#";
+    for(var i=0; i<6; ++i){
+        hex += letters[Math.floor(Math.random() * 16)];
+    }
+    mainSection.style.backgroundColor =  colorText.value = color.value = hex;
 }
