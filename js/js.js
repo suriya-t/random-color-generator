91240a52277c345a4e4fa4cabe3416b17989d0f7
suriya-t <86153076+suriya-t@users.noreply.github.com>
2021-08-28 21:49:53 +0530

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 const color = document.getElementById("color_input");
 const colorText = document.getElementById("color_text");
-const mainSection = document.getElementById("main-section");
+const mainSection = document.getElementById("main_section");
 
 function colorChange(){
     /* Change the color of the background and color code. */
